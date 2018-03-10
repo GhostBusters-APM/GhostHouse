7f33ed4acf3e96551e52f738fc66e8de2346256e
Alejandro Peral Taboada <alejandro.peral@udc.es>
2018-03-10 12:44:59 +0100

    Initial Commit

---
+++ Android/app/src/main/java/com/github/ghostbusters/ghosthouse/home/Home.java
@@ -0,0 +1,59 @@
+package com.github.ghostbusters.ghosthouse.home;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.github.ghostbusters.ghosthouse.R;
+import com.github.ghostbusters.ghosthouse.helper.view.BottomBarHelper;
+import com.github.ghostbusters.ghosthouse.home.fragments.ARFragment;
+import com.github.ghostbusters.ghosthouse.home.fragments.DataFragment;
+import com.github.ghostbusters.ghosthouse.home.fragments.HomeFragment;
+import com.github.ghostbusters.ghosthouse.home.fragments.UserFragment;
+
+public class Home extends AppCompatActivity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+
+		BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+		navigation.setOnNavigationItemSelectedListener(this::onNavigationBarItemSelected);
+		BottomBarHelper.disableShiftMode(navigation);
+		navigation.setSelectedItemId(R.id.navigation_home);
+	}
+
+	private boolean onNavigationBarItemSelected(@NonNull MenuItem item) {
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		FragmentTransaction transaction = fragmentManager.beginTransaction();
+		boolean result = changeMainView(item, transaction);
+		transaction.commit();
+		return result;
+	}
+
+	private boolean changeMainView(@NonNull MenuItem item, FragmentTransaction transaction) {
+		switch (item.getItemId()) {
+			case R.id.navigation_ar:
+				transaction.replace(R.id.home_base_frame, new ARFragment());
+				return true;
+			case R.id.navigation_home:
+				transaction.replace(R.id.home_base_frame, new HomeFragment());
+				return true;
+			case R.id.navigation_data:
+				transaction.replace(R.id.home_base_frame, new DataFragment());
+				return true;
+			case R.id.navigation_user:
+				transaction.replace(R.id.home_base_frame, new UserFragment());
+				return true;
+		}
+		return false;
+	}
+
+
+}
