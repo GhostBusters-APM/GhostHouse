b10fa52e1d35efc0d60c268c9afc17858dcd219f
Alejandro Peral Taboada <alejandro.peral@udc.es>
2018-04-09 23:32:05 +0200

    Endpoint for historic data

--- Cloud/src/main/kotlin/com/github/ghostbustersapm/ghosthouse/controller/GetController.kt
+++ Cloud/src/main/kotlin/com/github/ghostbustersapm/ghosthouse/controller/GetController.kt
@@ -2,12 +2,20 @@ package com.github.ghostbustersapm.ghosthouse.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalTime
 
 @RestController
 class GetController{
 
-
 @GetMapping("testget")
-fun get()="Hello World"
+fun get()="Hello World";
+
+data class Data(val hour:Int, val power:Int);
+
+@GetMapping("historic")
+fun getHistoric():List<Data> {
+	val now = LocalTime.now();
+	return (0..23).map { Data(it,now.minusHours(it.toLong()).hour)}
+}
 
 }
