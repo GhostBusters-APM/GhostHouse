86237f5cd5dc2f1497b93673c2e7286e76c59c8b
Brais Rey Souto <brais@MacBook-Pro-de-Brais.local>
2018-04-12 23:57:23 +0200

    Añadida llamada a servicio REST

--- Android/app/src/main/java/com/github/ghostbusters/ghosthouse/home/fragments/DataFragment.java
+++ Android/app/src/main/java/com/github/ghostbusters/ghosthouse/home/fragments/DataFragment.java
@@ -2,18 +2,31 @@ package com.github.ghostbusters.ghosthouse.home.fragments;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.ghostbusters.ghosthouse.R;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * A fragment with a Google +1 button.
@@ -55,33 +68,16 @@ public class DataFragment extends Fragment {
 							 final Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		final View view = inflater.inflate(R.layout.fragment_data, container, false);
-
-//		super.onCreate(savedInstanceState);
-//		view.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//		setContentView(R.layout.activity_main);
-
-		final GraphView line_graph = (GraphView) view.findViewById(R.id.graph);
-		final LineGraphSeries<DataPoint> line_series =
-				new LineGraphSeries<DataPoint>(new DataPoint[]{
-						new DataPoint(0, 100),
-						new DataPoint(1, 500),
-						new DataPoint(2, 300),
-						new DataPoint(3, 200),
-						new DataPoint(4, 600)
-				});
-
-		final StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(line_graph);
-		staticLabelsFormatter.setHorizontalLabels(new String[]{"Jan", "Feb", "March", "April", "May"});
-		line_graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
-
-		line_graph.addSeries(line_series);
-//		line_series.setOnDataPointTapListener(new OnDataPointTapListener() {
-//			@Override
-//			public void onTap(Series series, DataPointInterface dataPoint) {
-//				Toast.makeText(HomeFragment.this, "Series: On Data Point clicked: " + dataPoint, Toast.LENGTH_SHORT).show();
-//			}
-//		});
 		return view;
+	}
+
+
+	@Override
+	public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		new HttpRequestTask().execute();
+
 	}
 
 	@Override
@@ -127,5 +123,68 @@ public class DataFragment extends Fragment {
 		// TODO: Update argument type and name
 		void onFragmentInteraction(Uri uri);
 	}
+
+	private class HttpRequestTask extends AsyncTask<Void, Void, ArrayList<HashMap>> {
+		@Override
+		protected ArrayList<HashMap> doInBackground(Void... params) {
+			try {
+				final String url = "http://192.168.0.166:8080/historic";
+				RestTemplate restTemplate = new RestTemplate();
+				restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+				ObjectMapper oMapper = new ObjectMapper();
+				ResponseEntity<Object[]> responseEntity = restTemplate.getForEntity(url, Object[].class);
+				Object[] objects = responseEntity.getBody();
+				ArrayList<HashMap> list = new ArrayList();
+
+				for (int i = 0; i < objects.length; i++){
+					HashMap<String, Object> map = new HashMap<String, Object>();
+					map = oMapper.convertValue(objects[i], HashMap.class);
+					list.add(map);
+				}
+
+
+				return list;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+
+		@RequiresApi(api = Build.VERSION_CODES.N)
+		@Override
+		protected void onPostExecute(ArrayList<HashMap> list) {
+			final GraphView line_graph = (GraphView) DataFragment.this.getView().findViewById(R.id.graph);
+
+			List<DataPoint> dataPointsList = new ArrayList<DataPoint>();
+			for (int i = 0; i < list.size()-1; i++){
+				String hour = list.get(i).get("hour").toString();
+				String power = list.get(i).get("power").toString();
+
+				DataPoint dataPoint = new DataPoint(Integer.parseInt(hour), Integer.parseInt(power));
+
+				dataPointsList.add(dataPoint);
+			}
+
+			DataPoint[] dataPoints = new DataPoint[dataPointsList.size()];
+			dataPoints = dataPointsList.toArray(dataPoints);
+
+			final LineGraphSeries<DataPoint> line_series =
+					new LineGraphSeries<DataPoint>(dataPoints);
+
+			final StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(line_graph);
+
+
+			staticLabelsFormatter.setHorizontalLabels(new String[]{"0", "1", "2", "3", "4", "5","6","7",
+					"8","9","10","11","12","13","14","15", "16","17","18","19","20","21","22",
+					"23"});
+			line_graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+			line_graph.addSeries(line_series);
+
+	}
+
+}
+
 
 }
