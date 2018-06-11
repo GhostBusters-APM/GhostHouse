0e21e64ab409c10dc62646e822559f6aa327bfb6
Alfonso Landin <alfonso.landin@udc.es>
2018-06-11 18:03:56 +0200

    Fix broken commit.

---
+++ Android/app/src/main/java/com/github/ghostbusters/ghosthouse/home/fragments/DeviceListAdapter.java
@@ -0,0 +1,92 @@
+package com.github.ghostbusters.ghosthouse.home.fragments;
+
+import android.support.v7.util.DiffUtil;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.github.ghostbusters.ghosthouse.R;
+import com.github.ghostbusters.ghosthouse.db.Device;
+
+import java.util.List;
+
+public class DeviceListAdapter extends RecyclerView.Adapter<DeviceListAdapter.ViewHolder> {
+
+    private List<Device> data;
+
+    public void setDeviceList(final List<Device> devices) {
+        if (data == null) {
+            data = devices;
+            notifyItemRangeInserted(0, data.size());
+        } else {
+            final List<Device> oldData = data;
+            DiffUtil.DiffResult result = DiffUtil.calculateDiff(new DiffUtil.Callback() {
+                @Override
+                public int getOldListSize() {
+                    return oldData.size();
+                }
+
+                @Override
+                public int getNewListSize() {
+                    return devices.size();
+                }
+
+                @Override
+                public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                    return oldData.get(oldItemPosition).getDeviceId() ==
+                            devices.get(newItemPosition).getDeviceId();
+                }
+
+                @Override
+                public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                    return oldData.get(oldItemPosition).equals(devices.get(newItemPosition));
+                }
+            });
+            data = devices;
+            result.dispatchUpdatesTo(this);
+        }
+    }
+
+
+    @Override
+    public DeviceListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.device_type1_layout, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(DeviceListAdapter.ViewHolder holder, int position) {
+        final Device device = data.get(position);
+        holder.deviceNameTv.setText(device.getName());
+        holder.aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                Log.d(DeviceListAdapter.class.getSimpleName(), "checked changed: " + b);
+                Log.d(DeviceListAdapter.class.getSimpleName(), device.toString());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return data == null ? 0 : data.size();
+    }
+
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView deviceNameTv;
+        Switch aSwitch;
+
+        ViewHolder(View v) {
+            super(v);
+            deviceNameTv = v.findViewById(R.id.device_name_tv);
+            aSwitch = v.findViewById(R.id.device_sw);
+        }
+    }
+}
