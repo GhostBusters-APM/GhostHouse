b8a36a22199db78088189a2bc61b71f0f19684a8
César Miguel Santiago Núñez <cesar.santiago.nunhez@gmail.com>
2018-06-12 20:04:31 +0200

    Revert "Completed IoT"
    This reverts commit 15ba4e1a49c76a75a14a8409d041e48ab6d6730f.

--- Sonoff/communication.ino
+++ Sonoff/communication.ino
@@ -1,145 +1,83 @@
-#include "settings.h"
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <MQTT.h>
-#include <uMQTTBroker.h>
-
+#include "settings.h"
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+void connectServer() {
+ 
+  Serial.println();
+ 
+  client.setServer(SERVER, PORT);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect(DEVICE_ID)) {
+ 
+      Serial.println("connected");
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+}
+ 
 boolean sendConsumptionData(float kwh, char* from, char* to) {
-  boolean res;
 
+  connectServer();
+
+  boolean res;
+ 
   StaticJsonBuffer<500> JSONbuffer;
   JsonObject& JSONencoder = JSONbuffer.createObject();
-
-  JSONencoder["deviceId"] = deviceId;
+ 
+  JSONencoder["device"] = DEVICE_ID;
   JSONencoder["kwh"] = kwh;
   JSONencoder["from"] = from;
   JSONencoder["to"] = to;
-
+  
   char JSONmessageBuffer[100];
   JSONencoder.printTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-  Serial.println("Sending message HTTP server..");
+  Serial.println("Sending message to MQTT topic..");
   Serial.println(JSONmessageBuffer);
+ 
+  if (client.publish("consum", JSONmessageBuffer) == true) {
+    Serial.println("Success sending message");
+    res=true;
+  } else {
+    Serial.println("Error sending message");
+    res=false;
+  }
+ 
+  client.loop();
+  Serial.println("-------------");
+ 
+  delay(10000);
 
-  HTTPClient http;    //Declare object of class HTTPClient
+  client.disconnect();
 
-  http.begin("http://alexperal.com:8888/devicePower");      //Specify request destination
-  http.addHeader("Content-Type", "application/json");  //Specify content-type header
-
-  int httpCode = http.POST(JSONmessageBuffer);   //Send the request
-  String payload = http.getString();                  //Get the response payload
-
-  Serial.println(httpCode);   //Print HTTP return code
-  Serial.println(payload);    //Print request response payload
-
-  http.end();
   return res;
 }
 
-void callback(uint32_t *client, const char* topic, uint32_t topic_len, const char *data, uint32_t lengh) {
-  char topic_str[topic_len + 1];
-  os_memcpy(topic_str, topic, topic_len);
-  topic_str[topic_len] = '\0';
-
-  char data_str[lengh + 1];
-  os_memcpy(data_str, data, lengh);
-  data_str[lengh] = '\0';
-
-  if (!strcmp(topic_str, "REGISTER")) {
-    StaticJsonBuffer<500> jsonStringBuffer;
-    JsonObject& jsonString = jsonStringBuffer.parseObject(data_str);
-
-
-    const char* ssid = jsonString["ssid"];
-    const char* ssid_password = jsonString["password"];
-
-    Serial.println(ssid);
-    Serial.println(ssid_password);
-
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, ssid_password);
-
-    NETWORK = jsonString.get<String>("ssid");
-    SSID_PASSWORD = jsonString.get<String>("password");
-    userId = jsonString.get<String>("userId");
-    name = jsonString.get<String>("name");
-    latitude = jsonString.get<String>("latitude");
-    longitude = jsonString.get<String>("longitude");
-    type = 1;
-    state = false;
-    reg = false;
-
-
-    if (jsonString.success()) {
-      Serial.println("exito");
-    } else {
-      Serial.println("error");
-    }
-  } else if (!strcmp(topic_str, "SWITCH")) {
-    if (!strcmp(data_str, "ON")) {
-      Serial.println("encendido");
-      digitalWrite(REL_PIN, HIGH);
-      //      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-      //      digitalWrite(REL_PIN, !digitalRead(REL_PIN));
-    }
-    else if (!strcmp(data_str, "OFF")) {
-      Serial.println("apagado");
-      digitalWrite(REL_PIN, LOW);
-      //      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-      //      digitalWrite(REL_PIN, !digitalRead(REL_PIN));
-    }
-  } else if (!strcmp(topic_str, "ALIVE")) {
-
-    MQTT_local_publish((unsigned char *)"ALIVE/ACK", (unsigned char *)"alive", 5, 0, 0);
-    Serial.println("Sending alive");
+void callback(char* topic, byte* payload, unsigned int length) {
+  if ((char*)payload == "on") {
+    Serial.println("encendido");
+    digitalWrite(REL_PIN, HIGH);
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    digitalWrite(REL_PIN, !digitalRead(REL_PIN));
+  }
+  else if ((char*)payload == "off") {
+    Serial.println("apagado");
+    digitalWrite(REL_PIN, LOW);
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    digitalWrite(REL_PIN, !digitalRead(REL_PIN));
   }
 }
-
-void sendRegistration() {
-
-  Serial.println("registrado");
-  StaticJsonBuffer<500> jsonEncoderBuffer;
-  JsonObject& JSONencoder = jsonEncoderBuffer.createObject();
-
-  JSONencoder["userId"] = userId;
-  JSONencoder["name"] = name;
-  JSONencoder["latitude"] = latitude;
-  JSONencoder["longitude"] = longitude;
-  JSONencoder["type"] = type;
-  JSONencoder["state"] = state;
-  JSONencoder["ip"] = WiFi.localIP().toString();
-
-
-  char JSONmessageBuffer[1000];
-  JSONencoder.printTo(JSONmessageBuffer, sizeof(JSONmessageBuffer));
-  Serial.println("Sending message HTTP server..");
-  Serial.println(JSONmessageBuffer);
-
-  HTTPClient http;    //Declare object of class HTTPClient
-
-  http.begin("http://alexperal.com:8888/device");      //Specify request destination
-  http.addHeader("Content-Type", "application/json");  //Specify content-type header
-  Serial.println("chega aqui");
-
-  int httpCode = http.POST(JSONmessageBuffer);   //Send the request
-  String payload = http.getString();                  //Get the response payload
-  Serial.println("chega aqui tamen");
-
-  Serial.println(httpCode);   //Print HTTP return code
-  Serial.println(payload);    //Print request response payload
-
-  http.end();
-
-  StaticJsonBuffer<1000> jsonResponseBuffer;
-  JsonObject& jsonResponse = jsonResponseBuffer.parseObject(payload);
-
-  deviceId = jsonResponse["deviceId"];
-
-  Serial.println(deviceId);
-}
-
--- Sonoff/sonoff.ino
+++ Sonoff/sonoff.ino
@@ -1,4 +1,3 @@
-
 #include <TimeLib.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
@@ -7,9 +6,7 @@
 #include <PubSubClient.h>
 #include <Time.h>
 #include <Timezone.h>
-#include <MQTT.h>
-#include <uMQTTBroker.h>
-
+#include "settings.h"
 
 int16_t utc = 1;
 
@@ -20,18 +17,74 @@ TimeChangeRule CEST = {"CEST", Last, Sun, Mar, 2, 120};
 TimeChangeRule CET = {"CET", Last, Sun, Oct, 3, 60};
 Timezone CE(CEST, CET);
 
-IPAddress apLocalIP(192, 168, 0, 1);
-IPAddress subnet(255, 255, 255, 0);
-
 unsigned long lastReport;
 
 float kwh = 0.0;
 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  Serial.println("cheguei");
+
+  WiFi.mode(WIFI_STA);
+
+  WiFi.begin(NETWORK, SSID_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+
+  Serial.println("Connected to the WiFi network");
+
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(REL_PIN, OUTPUT);
+  pinMode(BUTTON, INPUT);
+
+  digitalWrite(LED_PIN, 1);
+  digitalWrite(REL_PIN, 0);
+
+
+  timeClient.begin();
+  timeClient.update();
+
+  lastReport = timeClient.getEpochTime();
+
+  client.subscribe("turn");
+  client.setCallback(callback);
+
+  hlw8012.begin(CF_PIN, CF1_PIN, SEL_PIN, CURRENT_MODE, false, 500000);
+}
+
+void loop() {
+  checkButton();
+  Serial.println(WiFi.localIP());
+
+  if (checkInterval()) {
+    sendConsumption();
+  }
+  //  if (!digitalRead(BUTTON))
+  //  {
+  //    Serial.println("cheguei");
+  //    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  //    digitalWrite(REL_PIN, !digitalRead(REL_PIN));
+  //    delay(100);
+  //    while (!digitalRead(BUTTON));
+  //    sendConsumption();
+  //  }
+  kwh = kwh + getEnergy();
+
+  Serial.println(kwh);
+
+  delay(500);
+
+}
+
+
 void checkButton() {
-  if (!digitalRead(BUTTON)) {
-    WiFi.softAPConfig(apLocalIP, apLocalIP, subnet);
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP("SONOFF001");
+  if (digitalRead(BUTTON)) {
+    WiFi.beginSmartConfig();
   }
 }
 
@@ -54,126 +107,12 @@ void sendConsumption() {
   char to[15];
   sprintf(to, "%d%02d%02d%02d%02d%02d", year(t), month(t), day(t), hour(t), minute(t), second(t));
   sprintf(from, "%d%02d%02d%02d%02d%02d", year(lr), month(lr), day(lr), hour(lr), minute(lr), second(lr));
-  Serial.println(from);
-  Serial.println(to);
+//  Serial.println(from);
+//  Serial.println(to);
   delay(1000);
   if (sendConsumptionData(kwh, from, to)) {
     lastReport = utc;
     kwh = 0;
   }
 }
-
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-
-  Serial.println("cheguei");
-
-  if (NETWORK != "") {
-    WiFi.mode(WIFI_STA);
-
-    char ssid[100];
-    char password[100];
-
-    NETWORK.toCharArray(ssid, 100);
-    SSID_PASSWORD.toCharArray(password, 100);
-    WiFi.begin(ssid, password);
-
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.println("Connecting to WiFi..");
-
-      Serial.println(NETWORK);
-    }
-    Serial.println(WiFi.status());
-    Serial.println(WiFi.localIP());
-    delay(500);
-    Serial.println("Connected to the WiFi network");
-  } else {
-    WiFi.softAPConfig(apLocalIP, apLocalIP, subnet);
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP("SONOFF001");
-  }
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(REL_PIN, OUTPUT);
-  pinMode(BUTTON, INPUT);
-
-  timeClient.begin();
-
-  if (WiFi.status() == WL_CONNECTED) {
-    while (!timeClient.update()) {
-
-      Serial.println("Updating time");
-    }
-  }
-  //  delay(5000);
-
-  lastReport = timeClient.getEpochTime();
-
-  hlw8012.begin(CF_PIN, CF1_PIN, SEL_PIN, CURRENT_MODE, false, 500000);
-
-  MQTT_server_onData(callback);
-  MQTT_server_start(1883, 30, 30);
-  MQTT_local_subscribe((unsigned char *)"#", 0);
-
-  digitalWrite(LED_PIN, 1);
-  digitalWrite(REL_PIN, 0);
-}
-
-void loop() {
-  checkButton();
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi..");
-
-    Serial.println(NETWORK);
-  }
-  if (!reg) {
-    sendRegistration();
-    if (WiFi.status() == WL_CONNECTED) {
-      while (!timeClient.update()) {
-
-        Serial.println("Updating time");
-      }
-    }
-    lastReport = timeClient.getEpochTime();
-    kwh = 0;
-    reg = true;
-  }
-  Serial.println("Connected to the WiFi network");
-  Serial.println(WiFi.status());
-  Serial.println(WiFi.localIP());
-  Serial.println(NETWORK);
-  Serial.println(SSID_PASSWORD);
-  Serial.println(deviceId);
-  Serial.println(name);
-  Serial.println(latitude);
-  Serial.println(longitude);
-  TimeChangeRule *tcr;
-  time_t t;
-  t = lastReport;
-  t = CE.toLocal(t, &tcr);
-  Serial.printf("%d%02d%02d%02d%02d%02d\n", year(t), month(t), day(t), hour(t), minute(t), second(t));
-
-
-
-  if (checkInterval()) {
-    sendConsumption();
-  }
-  //    if (!digitalRead(BUTTON))
-  //    {
-  //      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
-  //      digitalWrite(REL_PIN, !digitalRead(REL_PIN));
-  //      delay(100);
-  //      while (!digitalRead(BUTTON));
-  //      sendConsumption();
-  //    }
-  kwh = kwh + getEnergy();
-
-  Serial.println(kwh);
-
-  delay(500);
-
-}
-
 
