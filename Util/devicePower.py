d614f582768e75be815226bb291f7c2afa50a1cf
Alfonso Landin <alfonso.landin@udc.es>
2018-06-12 12:32:29 +0200

    Script to post random power data of a device at intervals.

---
+++ Util/devicePower.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+from urllib import request
+import json
+import datetime
+import sys
+import random
+import time
+
+URLFORMAT = 'http://{}:{}/devicePower'
+DATEFORMAT = '%Y%m%d%H%M%S'
+diferencia = datetime.timedelta(seconds=5)
+
+MENOR = 0.0
+MAYOR = 100.0
+
+def post_data(url, deviceID, value, then, now):
+    body = {'deviceID' : deviceID, 'value': value,
+            'from': then.strftime(DATEFORMAT),
+            'to': now.strftime(DATEFORMAT)}
+    bodybytes = json.dumps(body).encode('utf-8')
+    
+    req = request.Request(url)
+    req.add_header('Content-Type', 'application/json; charset=utf-8')
+    req.add_header('Content-Length', len(bodybytes))
+    res = request.urlopen(req, bodybytes)
+
+def post_loop(url, deviceId, interval):
+    while True:
+        now = datetime.datetime.now()
+        then = now - diferencia
+        value = random.uniform(MENOR, MAYOR)
+        post_data(url, deviceId, value, then, now)
+        time.sleep(interval)
+        
+    
+def main(argv):
+    if len(argv) != 5:
+        print('Usage: %s <host> <port> <deviceId> <interval>'.format(argv[0]))
+        return
+    
+    host = argv[1]
+    port = argv[2]
+    deviceId = int(argv[3])
+    interval = int(argv[4])
+    
+    url = URLFORMAT.format(host, port)
+
+    post_loop(url, deviceId, interval)
+
+if __name__ == '__main__':
+    main(sys.argv)
+
